c718dab5681c36a5194787ee5908357d7d2c52ba
Benjamin Taylor <staylor.ben@gmail.com>
2019-04-04 12:52:34 -0700

    Created models, dbcontext & data migrations

---
+++ AsyncInn/AsyncInn/Models/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Hotel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+
+        public HotelRoom HotelRoom { get; set; }
+    }
+}
