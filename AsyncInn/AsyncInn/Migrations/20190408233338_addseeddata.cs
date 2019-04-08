4101d7fed2bbf7ec7f030036b413f97921156664
Benjamin Taylor <staylor.ben@gmail.com>
2019-04-08 16:40:09 -0700

    Added seed data

---
+++ AsyncInn/AsyncInn/Migrations/20190408233338_addseeddata.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class addseeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Hot Tub" },
+                    { 2, "Mini Bar" },
+                    { 3, "Home Theater" },
+                    { 4, "Virtual Reality Headset" },
+                    { 5, "Dance Floor" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "ID", "City", "Name", "Phone", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Seattle", "Async Seattle", "(206) 226-2606", "WA", "123 Easy St" },
+                    { 2, "Portland", "Async Portland", "(503) 553-5303", "OR", "456 Medium Ave" },
+                    { 3, "Los Angeles", "Async Angels", "(213) 223-2313", "CA", "789 Hard Blvd" },
+                    { 4, "Chicago", "Chi Town Async Inn", "(217) 227-2717", "IL", "1234 Main St" },
+                    { 5, "Miami", "Hotline Async", "(305) 335-3505", "FL", "5678 Primary Dr" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "ID", "Layout", "Name" },
+                values: new object[,]
+                {
+                    { 1, 0, "Cozy Studio" },
+                    { 2, 0, "All-Star Studio" },
+                    { 3, 1, "1 Bedroom Wonder" },
+                    { 4, 1, "1 Bedroom To Rule Them All" },
+                    { 5, 2, "2 Bedrooms 2 Furious" },
+                    { 6, 2, "The Grandmaster's Suite" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "ID",
+                keyValue: 6);
+        }
+    }
+}
