dc17b7ca70846c47e3bc8a6f5fe14920631b35ea
Benjamin Taylor <staylor.ben@gmail.com>
2019-04-08 19:13:25 -0700

    Added navbar

--- AsyncInn/AsyncInn/Models/HotelRoom.cs
+++ AsyncInn/AsyncInn/Models/HotelRoom.cs
@@ -11,12 +11,16 @@ namespace AsyncInn.Models
     {
         [Key]
         [ForeignKey("Hotel")]
+        [Display(Name = "Hotel")]
         public int HotelID { get; set; }
         [Key]
+        [Display(Name = "Room Number")]
         public int RoomNumber { get; set; }
         [ForeignKey("Room")]
+        [Display(Name = "Room Type")]
         public int RoomID { get; set; }
         public decimal Rate { get; set; }
+        [Display(Name = "Pet Friendly")]
         public bool PetFriendly { get; set; }
         
         public Hotel Hotel { get; set; }
